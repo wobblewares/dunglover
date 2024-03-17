bb4c6ad7b45e292a6a384556f1959888128f2266
ian <ian@wobblewares.com>
2024-03-18 07:20:47 +1100

    Update WobbleShadow to use collider if desired

--- Packages/com.wobblewares.kit/Core/WobbleShadow/Shared/Scripts/WobbleShadow.cs
+++ Packages/com.wobblewares.kit/Core/WobbleShadow/Shared/Scripts/WobbleShadow.cs
@@ -10,6 +10,13 @@ namespace Wobblewares.Kit
     {
         #region Inspector Variables
 
+        public enum BoundsMode
+        {
+            MeshRenderer,
+            Collider
+        }
+        
+        public BoundsMode boundsMode = BoundsMode.MeshRenderer;
         public Transform target;
         public float offset = 0.1f;
         public Vector3 direction = Vector3.down;
@@ -27,17 +34,29 @@ namespace Wobblewares.Kit
         {
             if (target == null)
                 target = transform.parent;
-    
+
+            RefreshReferences();
             UpdateShadowPosition();
         }
 
-
-
         private void Update()
         {
-         
             UpdateShadowPosition();
-          
+        }
+
+        private void OnValidate()
+        {
+            if (target == null) return;
+            
+            // rename automatically
+            gameObject.name = $"WobbleShadow - {target.name}";
+            RefreshReferences();
+        }
+
+        private void RefreshReferences()
+        {
+            _meshRenderer = target.GetComponent<MeshRenderer>();
+            _collider = target.GetComponent<Collider>();
         }
 
         private void OnDrawGizmosSelected()
@@ -52,31 +71,45 @@ namespace Wobblewares.Kit
             UpdateShadowPosition();
         }
 
+        private Bounds GetBounds()
+        {
+            if (target == null)
+                return new Bounds();
+            
+            switch (boundsMode) 
+            {
+                case BoundsMode.MeshRenderer:
+                    if (_meshRenderer == null) return new Bounds();
 
+                    if (!useLocalBounds)
+                        return _meshRenderer.bounds;
+                    Bounds bounds = _meshRenderer.localBounds;
+                    bounds.center += target.transform.position;
+                    bounds.size = new Vector3(bounds.size.x * target.transform.lossyScale.x,
+                        bounds.size.y * target.transform.lossyScale.y,
+                        bounds.size.z * target.transform.lossyScale.z);
+                    return bounds;
+                case BoundsMode.Collider:
+                    if (_collider == null) return new Bounds();
+                    return _collider.bounds;
+            }
+            return new Bounds();
+        }
+        
         private void UpdateShadowPosition()
         {
             if (target == null)
                 return;
 
-            if (useLocalBounds)
-            {
-                bounds = target.GetComponent<MeshRenderer>().localBounds;
-                bounds.center += target.transform.position;
-                bounds.size = new Vector3(bounds.size.x * target.transform.lossyScale.x,
-                    bounds.size.y * target.transform.lossyScale.y,
-                    bounds.size.z * target.transform.lossyScale.z);
-            }
-            else
-            {
-                bounds = target.GetComponent<MeshRenderer>().bounds;
-            }
-   
+            bounds = GetBounds();
             transform.position = new Vector3(bounds.center.x, (bounds.center.y - bounds.size.y / 2.0f) - offset, bounds.center.z);
             transform.rotation = Quaternion.LookRotation(Vector3.down, target.transform.forward);
         }
         
 
         private Bounds bounds;
+        private MeshRenderer _meshRenderer = null;
+        private Collider _collider = null;
 
         #endregion
     }
